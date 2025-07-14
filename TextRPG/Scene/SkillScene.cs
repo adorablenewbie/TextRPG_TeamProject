f6c4fbad2880d9c0aea893d3c51b0463f01f6db6
pcw30215 <scout386@naver.com>
2025-07-14 20:30:59 +0900

    [ feat ] 스킬 화면 임시 구성

--- TextRPG/Scene/SkillScene.cs
+++ TextRPG/Scene/SkillScene.cs
@@ -9,81 +9,38 @@ namespace TextRPG.Scene
 {
     internal class SkillScene
     {
-        static void DisplaySkillUI()
+        static void ShowSkillUI()
         {
             Console.Clear();
             Console.WriteLine("스킬");
-            Console.WriteLine("보유 중인 스킬을 관리할 수 있습니다.");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
             Console.WriteLine();
             Console.WriteLine("[스킬 목록]");
 
-            //for (int i = 0; i < inventory.Count; i++) // 임시 for
-            //{
-            //    int targetItem = inventory[i];
+            for (int i = 0; i < skill.Count; i++) // 임시로 단어를 inventory에서 skill로 변경
+            {
+                int targetSkill = skill[i];
 
-            //    string displayEquipped = equipList.Contains(targetItem) ? "[E]" : "";
-            //    Console.WriteLine($"- {displayEquipped} {itemNames[targetItem]}  |  {(itemType[targetItem] == 0 ? "공격력" : "방어력")} +{itemValue[targetItem]}  |  {itemDesc[targetItem]}");
-            //}
+                string displayEquipped = equipList.Contains(targetSkill) ? "[E]" : "";
+                Console.WriteLine($"- {displayEquipped} {skillNames[targetSkill]}  |  {(skillType[targetSkill] == 0 ? "공격력" : "방어력")} +{itemValue[targetItem]}  |  {itemDesc[targetItem]}");
+            }
             Console.WriteLine();
             Console.WriteLine("1. 장착 관리");
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
             string input = Console.ReadLine();
-        }
 
-        static void DisplayEquipSkillUI()
-        {
-            Console.Clear();
-            Console.WriteLine("스킬 - 장착관리");
-            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
-            Console.WriteLine();
-            Console.WriteLine("[아이템 목록]");
+            switch (result)
+            {
+                case 0: // 메인 메뉴로 돌아가기
+                    ShowMainMenu();
+                    break;
 
-            //for (int i = 0; i < inventory.Count; i++) // 임시 for
-            //{
-            //    int targetItem = inventory[i];
-
-            //    string displayEquipped = equipList.Contains(targetItem) ? "[E]" : "";
-            //    Console.WriteLine($"- {i + 1} {displayEquipped} {itemNames[targetItem]}  |  {(itemType[targetItem] == 0 ? "공격력" : "방어력")} +{itemValue[targetItem]}  |  {itemDesc[targetItem]}");
-            //}
-            Console.WriteLine();
-            Console.WriteLine("0. 나가기");
-            Console.WriteLine();
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
-            string input = Console.ReadLine();
-
-            //switch (result) // 임시 switch
-            //{
-            //    case 0:
-            //        DisplayInventoryUI();
-            //        break;
-
-            //    default:
-
-            //        int targetItem = result - 1;
-            //        bool isEquipped = equipList.Contains(targetItem);
-
-            //        if (isEquipped)
-            //        {
-            //            equipList.Remove(targetItem);
-            //            if (itemType[targetItem] == 0)
-            //                extraAtk -= itemValue[targetItem];
-            //            else
-            //                extraDef -= itemValue[targetItem];
-            //        }
-            //        else
-            //        {
-            //            equipList.Add(targetItem);
-            //            if (itemType[targetItem] == 0)
-            //                extraAtk += itemValue[targetItem];
-            //            else
-            //                extraDef += itemValue[targetItem];
-            //        }
-
-            //        DisplayEquipUI();
-            //        break;
-            //}
+                case 1: // 스킬 장착 관리 (미구현)
+                    ShowEquipSkill();
+                    break;
+            }
         }
     }
 }
