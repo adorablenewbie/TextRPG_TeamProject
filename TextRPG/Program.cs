99b0878f49a90b26f9348040205ceba9cc1aac85
broccoli1962 <aq0011@naver.com>
2025-07-14 19:41:39 +0900

    [ feat ] Program.cs
    [ feat ] Program.cs

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextRPG.Old;
+using TextRPG.Scene;
 
 namespace TextRPG
 {
     internal class Program
     {
+        private Dictionary<string, TextRPG.Scene.Scene> scenes;
+        private TextRPG.Scene.Scene currentScene;
+
+        public void initScenes()
+        {
+            //scenes.Add("MainMenu", new MainMenu());
+
+        }
+
         public static void Main(string[] args)
         {
-
+            TextRPG.Scene.Scene statusScene = new StatusScene();
+            statusScene.ShowScene();
         }
     }
 }
