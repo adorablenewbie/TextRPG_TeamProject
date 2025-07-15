de10c16edb4ada76534fad85198f26eda66af5b1
broccoli1962 <aq0011@naver.com>
2025-07-15 09:24:08 +0900

    [ chore ] 네임스페이스 수정3
    [ chore ] 네임스페이스 수정3

--- TextRPG/Scenes/SkillScene.cs
+++ TextRPG/Scenes/SkillScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TextRPG.SaveData;
+using TextRPG.SaveDatas;
 
 namespace TextRPG.Scenes
 {
