90d011927b5d150ef602275b1a3e1a57263f8f59
pcw30215 <scout386@naver.com>
2025-07-15 14:51:46 +0900

    [ feat ] 스킬 장착 관리 추가 및 단어 수정

--- TextRPG/Scenes/SkillScene.cs
+++ TextRPG/Scenes/SkillScene.cs
@@ -9,11 +9,11 @@ namespace TextRPG.Scenes
 {
     internal class SkillScene
     {
-        static void ShowSkillUI()
+        static void ShowSkill()
         {
             Console.Clear();
             Console.WriteLine("스킬");
-            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine("보유 중인 스킬을 관리할 수 있습니다.");
             Console.WriteLine();
             Console.WriteLine("[스킬 목록]");
 
@@ -37,7 +37,40 @@ namespace TextRPG.Scenes
                     ShowMainMenu();
                     break;
 
-                case 1: // 스킬 장착 관리 (미구현)
+                case 1: // 스킬 장착 관리
+                    ShowEquipSkill();
+                    break;
+            }
+        }
+
+        static void ShowEquipSkill()
+        {
+            Console.Clear();
+            Console.WriteLine("스킬 - 장착관리");
+            Console.WriteLine("보유 중인 스킬을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[스킬 목록]");
+
+            player.ShowSkill(true);
+
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            string input = Console.ReadLine();
+
+            switch (result) // 임시로 단어를 item에서 skill로 변경
+            {
+                case 0:
+                    ShowSkill();
+                    break;
+
+                default:
+
+                    int skillIdx = result - 1;
+                    Skill targetSkill = skillDb[skillIdx];
+                    player.EquipSkill(targetSkill);
+
                     ShowEquipSkill();
                     break;
             }
