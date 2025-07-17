3bf66fe9daa1f27a0f609c8da882ad38e84a6139
broccoli1962 <aq0011@naver.com>
2025-07-17 11:56:20 +0900

    [ feat ] 퀘스트 씬 작업 1차
    [ feat ] 퀘스트 씬 작업 1차

---
+++ TextRPG/SaveDatas/Quest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG.SaveDatas
+{
+    public class Quest
+    {
+        private string questDescript;
+
+        public static List<Quest> questList = new()
+        {
+            new Quest("마을을 위협하는 미니언 처치"),
+            new Quest("장비를 장착해보기"),
+            new Quest("더욱 더 강해지기"),
+        };
+        public Quest(string descript)
+        {
+            this.questDescript = descript;
+        }
+
+        public void ChooseQuest()
+        {
+
+        }
+
+        public void ShowQuestList()
+        {
+            Console.WriteLine(this.questDescript);
+        }
+    }
+}
--- TextRPG/Scenes/QuestScene.cs
+++ TextRPG/Scenes/QuestScene.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextRPG.SaveDatas;
 
 namespace TextRPG.Scenes
 {
@@ -10,7 +11,27 @@ namespace TextRPG.Scenes
     {
         public override void ShowScene()
         {
+            Console.Clear();
+            Console.WriteLine("Quest!!");
+            Console.WriteLine();
+            for (int i = 0; i < Quest.questList.Count; i++) {
+                Quest.questList[i].ShowQuestList();
+            }
+            Console.WriteLine("원하시는 퀘스트를 선택해주세요.");
+            Console.WriteLine(">>");
             
+            string Input = Console.ReadLine();
+            bool isNumber = int.TryParse(Input, out int result);
+            
+            if(result == 0)
+            {
+                Program.ChangeScene(SceneType.MainScene);
+            }
+            else
+            {
+                int selectedNum = result - 1;
+                //QuestList[selectedNum].ChooseQuest();
+            }
         }
     }
 }
