abbe25e84368f0774e590be862f7632248d2a87f
adorablenewbie <youngjong2965@gmail.com>
2025-07-17 17:35:38 +0900

    [ feat ] json 세이브로드

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -80,11 +80,11 @@ namespace TextRPG
             }
             Console.ResetColor();
         }
-        public void SavePlayer()
+        public static void SavePlayer()
         {
             try
             {
-                string json = JsonSerializer.Serialize(player);
+                string json = JsonSerializer.Serialize(Player.Instance);
                 File.WriteAllText(SaveFileName, json);
                 Console.WriteLine("캐릭터가 저장되었습니다.");
                 System.Threading.Thread.Sleep(1000);
@@ -95,5 +95,54 @@ namespace TextRPG
                 System.Threading.Thread.Sleep(1000);
             }
         }
-    }
+        public Player LoadOrCreatePlayer()
+        {
+            while (true)
+            {
+                if (File.Exists(SaveFileName))
+                {
+                    Console.WriteLine("저장된 캐릭터가 있습니다. 불러오시겠습니까? (y: 불러오기, n: 새로 만들기)");
+                    string input = Console.ReadLine();
+                    if (input == "y")
+                    {
+                        try
+                        {
+                            Console.Clear();
+                            string json = File.ReadAllText(SaveFileName);
+                            Player loaded = JsonSerializer.Deserialize<Player>(json);
+
+                            if (loaded != null)
+                            {
+                                Console.WriteLine($"[{loaded.Name}] 캐릭터를 불러왔습니다.");
+                                loaded.Initialize();
+                                return loaded;
+                            }
+                        }
+                        catch
+                        {
+                            Console.Clear();
+                            Console.WriteLine("저장된 캐릭터를 불러오는 데 실패했습니다. 새로 만듭니다.");
+                        }
+                    }
+                    else if (input == "n")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("새로운 캐릭터를 생성합니다.");
+                        return null;
+                    }
+                    else
+                    {
+                        Console.WriteLine("잘못된 입력입니다.");
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("새로운 캐릭터를 생성합니다.");
+                    return null;
+                }
+            }
+        }
+     }
 }
