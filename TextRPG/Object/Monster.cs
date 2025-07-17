3ace521924f63f14b492109214863557c1d42201
adorablenewbie <youngjong2965@gmail.com>
2025-07-17 16:44:41 +0900

    [ chore ] 메인메뉴, 몬스터 주석제거

--- TextRPG/Object/Monster.cs
+++ TextRPG/Object/Monster.cs
@@ -42,18 +42,4 @@ namespace TextRPG.Object
             this.Inventory = new List<Item>();
         }
     }
-
-    //public class MonsterManager
-    //{
-    //    public Monster[] Monsters = new Monster[5];
-
-    //    public MonsterManager()
-    //    {                                     //hp,공격,방어,골드,EXP
-    //        Monsters[0] = new Monster("고블린", 30, 2, 1, 10, 10);
-    //        Monsters[1] = new Monster("슬라임", 20, 4, 0, 20, 20);
-    //        Monsters[2] = new Monster("스켈레톤", 40, 6, 0, 30, 30);
-    //        Monsters[3] = new Monster("오크", 50, 8, 3, 50, 50);
-    //        Monsters[4] = new Monster("드래곤", 200, 20, 5, 100, 500);
-    //    }
-    //}
 }
