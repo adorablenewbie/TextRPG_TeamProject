7e5c4e7db8560eb1c27322a03e235b5a9fc4f4b5
broccoli1962 <aq0011@naver.com>
2025-07-18 10:01:31 +0900

    [ feat ] 몬스터 레벨 추가
    [ feat ] 몬스터 레벨 추가

--- TextRPG/Object/Monster.cs
+++ TextRPG/Object/Monster.cs
@@ -14,19 +14,19 @@ namespace TextRPG.Object
 
         public static List<Monster> monstersData = new List<Monster>()
         {
-            new Monster("슬라임", 10, 2, 0, 10, 10, DungeonType.Forest),
-            new Monster("고블린", 20, 3, 1, 15, 15, DungeonType.Forest),
-            new Monster("산적", 25, 3, 2, 25, 25, DungeonType.Forest),
-            new Monster("늑대인간", 30, 4, 2, 30, 30, DungeonType.Forest),
-            new Monster("박쥐", 35, 5, 2, 40, 40, DungeonType.Cave),
-            new Monster("좀비", 50, 5, 3, 50, 50, DungeonType.Cave),
-            new Monster("스켈레톤", 60, 6, 3, 60, 60, DungeonType.Cave),
-            new Monster("오크", 75, 8, 4, 75, 75, DungeonType.Cave),
-            new Monster("미믹", 80, 10, 5, 80, 80, DungeonType.Castle),
-            new Monster("뱀파이어", 100, 10, 5, 100, 100, DungeonType.Castle),
-            new Monster("흑마법사", 120, 12, 5, 120, 120, DungeonType.Castle),
-            new Monster("흑기사", 150, 15, 7, 150, 150, DungeonType.Castle),
-            new Monster("흑염룡", 500, 25, 10, 300, 300, DungeonType.DragonLair)
+            new Monster(1 ,"슬라임", 10, 2, 0, 10, 10, DungeonType.Forest),
+            new Monster(1 ,"고블린", 20, 3, 1, 15, 15, DungeonType.Forest),
+            new Monster(3 ,"산적", 25, 3, 2, 25, 25, DungeonType.Forest),
+            new Monster(4 ,"늑대인간", 30, 4, 2, 30, 30, DungeonType.Forest),
+            new Monster(2 ,"박쥐", 35, 5, 2, 40, 40, DungeonType.Cave),
+            new Monster(1 ,"좀비", 50, 5, 3, 50, 50, DungeonType.Cave),
+            new Monster(2 ,"스켈레톤", 60, 6, 3, 60, 60, DungeonType.Cave),
+            new Monster(4 ,"오크", 75, 8, 4, 75, 75, DungeonType.Cave),
+            new Monster(3 ,"미믹", 80, 10, 5, 80, 80, DungeonType.Castle),
+            new Monster(5 ,"뱀파이어", 100, 10, 5, 100, 100, DungeonType.Castle),
+            new Monster(7 ,"흑마법사", 120, 12, 5, 120, 120, DungeonType.Castle),
+            new Monster(8 ,"흑기사", 150, 15, 7, 150, 150, DungeonType.Castle),
+            new Monster(20 ,"흑염룡", 500, 25, 10, 300, 300, DungeonType.DragonLair)
         };
 
         public Monster Clone()
@@ -34,8 +34,9 @@ namespace TextRPG.Object
             return new Monster(this.Name, this.Hp, this.BaseAttack, this.BaseDefense, this.Gold, this.Exp, this.DungeonType);
         }
 
-        public Monster(string name, float hp, float attack, float defense, float gold, float exp, DungeonType dungeonType)
+        public Monster(int level, string name, float hp, float attack, float defense, float gold, float exp, DungeonType dungeonType)
         {
+            this.Level = level;
             this.Name = name;
             this.Hp = hp;
             this.BaseAttack = attack;
